91108a5446c11dc94be4de4ad2acceb4e7b34b48
Danish <danishsidd524@gmail.com>
2024-07-23 17:40:58 +0500

    add: group api calls

--- API/chats.api.ts
+++ API/chats.api.ts
@@ -76,4 +76,148 @@ export const sendChat = async ({
   }
 };
 
+export const createGroup = async ({
+  groupName,
+  members,
+}: {
+  members: string[];
+  groupName: string;
+}) => {
+  try {
+    const { data } = await api.post(`/chats/group`, {
+      groupName,
+      members,
+    });
 
+    if (!data.success)
+      return {
+        success: false,
+        response: "Something went wrong",
+      };
+
+    return {
+      success: true,
+      response: data.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      response: error?.response?.data?.message || "Something went wrong",
+    };
+  }
+};
+
+export const updateGroup = async ({
+  groupName,
+  groupDescription,
+  chatId,
+}: {
+  groupDescription: string;
+  groupName: string;
+  chatId: string;
+}) => {
+  try {
+    const { data } = await api.post(`/chats/group/${chatId}`, {
+      groupName,
+      groupDescription,
+    });
+
+    if (!data.success)
+      return {
+        success: false,
+        response: "Something went wrong",
+      };
+
+    return {
+      success: true,
+      response: data.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      response: error?.response?.data?.message || "Something went wrong",
+    };
+  }
+};
+
+export const leaveGroup = async (chatId: string) => {
+  try {
+    const { data } = await api.put(`/chats/group/${chatId}/leave`);
+
+    if (!data.success)
+      return {
+        success: false,
+        response: "Something went wrong",
+      };
+
+    return {
+      success: true,
+      response: data.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      response: error?.response?.data?.message || "Something went wrong",
+    };
+  }
+};
+
+export const addMembers = async ({
+  chatId,
+  members,
+}: {
+  chatId: string;
+  members: string[];
+}) => {
+  try {
+    const { data } = await api.put(`/chats/group/${chatId}/members/add`, {
+      members,
+    });
+
+    if (!data.success)
+      return {
+        success: false,
+        response: "Something went wrong",
+      };
+
+    return {
+      success: true,
+      response: data.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      response: error?.response?.data?.message || "Something went wrong",
+    };
+  }
+};
+
+export const removeMembers = async ({
+  chatId,
+  members,
+}: {
+  chatId: string;
+  members: string[];
+}) => {
+  try {
+    const { data } = await api.put(`/chats/group/${chatId}/members/remove`, {
+      members,
+    });
+
+    if (!data.success)
+      return {
+        success: false,
+        response: "Something went wrong",
+      };
+
+    return {
+      success: true,
+      response: data.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      response: error?.response?.data?.message || "Something went wrong",
+    };
+  }
+};
